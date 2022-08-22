aca28222327fe2dccbe6d1e569f2eb87c20df9fe
Andrew Ward <redrawdawn@gmail.com>
2022-08-22 11:12:32 -0500

    phase 6

--- Program.cs
+++ Program.cs
@@ -11,8 +11,11 @@ while(attempts > 0)
     attempts--;
     int guessedNumber = int.Parse(Console.ReadLine());
 
+    string highOrLow = (guessedNumber > secretNumber)
+     ? "Too high" : "Too low";
+
     string onWrong = (attempts != 0) 
-    ? $"Try again... You have {attempts} tries left" 
+    ? $"{highOrLow}, You have {attempts} tries left" 
     : $"All out of tries,\nThe number was {secretNumber}";
 
     bool correct = guessedNumber == secretNumber;
