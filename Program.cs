c582523ad7a2c9bf4e758fd2de8c214682437f55
Andrew Ward <redrawdawn@gmail.com>
2022-08-22 11:05:32 -0500

    phase 5

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 Console.Write("Guess a secret number: ");
 
-int secretNumber = 42;
+int secretNumber = new Random().Next(1, 101);
 
 int attempts = 4;
 
@@ -11,7 +11,9 @@ while(attempts > 0)
     attempts--;
     int guessedNumber = int.Parse(Console.ReadLine());
 
-    string onWrong = (attempts != 0) ? $"Try again... You have {attempts} tries left" : "All out of tries";
+    string onWrong = (attempts != 0) 
+    ? $"Try again... You have {attempts} tries left" 
+    : $"All out of tries,\nThe number was {secretNumber}";
 
     bool correct = guessedNumber == secretNumber;
 
