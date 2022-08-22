89c4def3733d9d3ee33b8af424f65242d2257f9c
Andrew Ward <redrawdawn@gmail.com>
2022-08-22 10:30:20 -0500

    phase 1 and 2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+Console.Write("Guess a secret number: ");
+int guessedNumber = int.Parse(Console.ReadLine());
+int secretNumber = 42;
+string result = (guessedNumber == secretNumber)
+? "you guessed tje secret number" : "try again";
+Console.WriteLine(result);
